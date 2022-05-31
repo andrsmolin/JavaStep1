dfc23893592a1a02cbbc4b65a58d72af20e977f9
asmolin <andrei.smolin@pmi.com>
2022-05-31 20:16:02 +0300

    Create Employee.java

---
+++ Lesson5/Employee.java
@@ -0,0 +1,27 @@
+package Lesson5;
+
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    public Employee(String name, String position, String email, String phone, int salary, int age){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void info() {
+            System.out.printf("Cотрудник %s занимает должность %s c окладом в %d $. Ему %d лет." +
+                    "Контакты - %s, %s%n", name, position, salary, age, email, phone);
+        }
+    public int getAge() {
+        return age;
+    }
+}
